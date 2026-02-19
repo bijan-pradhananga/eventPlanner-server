829baf4f7aa2c829a1f91a4da25d6e2535d62265
bijan_pradhananga <bijan2059@gmail.com>
2026-02-19 08:21:01 +0545

    feat: add error handling utilities with asyncHandler and AppError class

---
+++ src/utils/errorUtils.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const asyncHandler = (fn: Function) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+};
+
+export class AppError extends Error {
+  public statusCode: number;
+  public isOperational: boolean;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export const createError = (message: string, statusCode: number = 500): AppError => {
+  return new AppError(message, statusCode);
+};
